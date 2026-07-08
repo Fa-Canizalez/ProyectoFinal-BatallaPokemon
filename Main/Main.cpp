07e76ad2fa48ec4ec4a9acb7346bbeebda487d05
Fa-Canizalez <00023726@uca.edu.sv>
2026-07-08 03:03:38 -0600

    :memo: Agregando Cuerpo del MENU PRINCIPAL

--- Main/Main.cpp
+++ Main/Main.cpp
@@ -47,5 +47,41 @@ int main ()
     pausar(); //Detiene la ejecucion del programa mientras espera una accion del usuario
 
 
+while (opcionMenu != 3)
+{
+    opcionMenu = GenerarMenu(); //Funcion para generar el Menu incial dl juego
+
+    switch (opcionMenu)
+    {
+        case 1: //Nueva Partida
+            NombrePartida = NuevaPartida(); //Funcion para la creación de una partida nueva
+
+            //Funciones para la creacion de los perfiles de cada jugador
+            PerfilJugador (1, Jugador1); 
+            SeleccionEquipo(Jugador1, Equipo1, Vida1, 6);
+            
+            PerfilJugador (2, Jugador2);
+            SeleccionEquipo(Jugador2, Equipo2, Vida2, 6);
+
+            juego = true; // Se cambia el estado del juego para pasar a la batalla
+
+            cout << "\n---AUTOGUARDADO---\n";
+            GuardarPartida(NombrePartida, Jugador1, Equipo1, Vida1, PokemonActual1, Jugador2, Equipo2, Vida2, PokemonActual2, turno); //Funcion para guardar la nueva partida
+            break;
+
+        case 2: 
+            if (CargarPartida (NombrePartida, Jugador1, Equipo1, Vida1, PokemonActual1, Jugador2, Equipo2, Vida2, PokemonActual2, turno)) //Funcion para cargar una partida 
+            {
+                juego = true; // La carga correcta de la partida cambia el estado del juego para ir a la batalla
+            }
+            break;
+
+        case 3: //Salir
+            cout << "\nSaliendo del juego...\nHasta la proxima!\n";
+            juego = false; //Se cambia el estado del juego para salir del bucle y salir del juego
+        break;
+    }
+}
+
     return 0;
 }
